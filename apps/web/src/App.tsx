07053013c31a76dbf1fc69bf725be835dbbef17a
HwangGoeun <hwang_gim@naver.com>
2025-12-22 15:59:14 +0900

    chore: Set up Zustand environment

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,7 +1,19 @@
+import { useStore } from './stores/useCounterStore';
 import './index.css';
 
 function App() {
-  return <h1 className="underline">Hello Wandok!</h1>;
+  const { count, inc } = useStore();
+
+  return (
+    <>
+      <h1 className="underline">Hello Wandok!</h1>
+
+      <div>
+        <span>{count}</span>
+        <button onClick={inc} className="bg-blue-300 ml-2">one up</button>
+      </div>
+    </>
+  );
 }
 
 export default App;
