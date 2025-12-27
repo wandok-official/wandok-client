dcc8226bdb76ded17dd185efc570e104393ff61a
Keunwoo <104286190+kw9212@users.noreply.github.com>
2025-12-27 23:23:55 +0900

    feat: Add Google social login page

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,19 +1,8 @@
-import { useStore } from './stores/useCounterStore';
+import LoginPage from './pages/LoginPage';
 import './index.css';
 
 function App() {
-  const { count, inc } = useStore();
-
-  return (
-    <>
-      <h1 className="underline">Hello Wandok!</h1>
-
-      <div>
-        <span>{count}</span>
-        <button onClick={inc} className="bg-blue-300 ml-2">one up</button>
-      </div>
-    </>
-  );
+  return <LoginPage />;
 }
 
 export default App;
