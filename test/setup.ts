ba1cf5f95295ff15ccc34929029d4d0e848ff654
HwangGoeun <hwang_gim@naver.com>
2026-01-21 23:24:47 +0900

    chore: @testing-library/jest-dom 설정 및 TypeScript 구성 개선

--- test/setup.ts
+++ test/setup.ts
@@ -1,6 +1,8 @@
+/// <reference types="@testing-library/jest-dom" />
 import { vi, beforeAll, afterAll, afterEach } from 'vitest';
 import { cleanup } from '@testing-library/react';
 import '@testing-library/dom';
+import '@testing-library/jest-dom/vitest';
 
 import { mockChrome } from './mocks/chrome';
 
